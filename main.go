a87608a1652a47566ec5a534a7454b186e2ee48c
juntaki <me@juntaki.com>
2018-12-19 00:59:31 +0900

    csv

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"sort"
+	"strings"
 
 	"github.com/ikawaha/kagome/tokenizer"
 	"golang.org/x/text/unicode/norm"
 )
 
 func main() {
-	input := "ＡＢＣ  -1ﾍﾟﾝｷﾞﾝ"
-	input = string(norm.NFKC.Bytes([]byte(input)))
-
 	dic, err := tokenizer.NewUserDic("./dic.csv")
 	if err != nil {
 		panic(err)
@@ -18,17 +19,68 @@ func main() {
 	t := tokenizer.New()
 	t.SetUserDic(dic)
 
-	output := []string{}
-	tokens := t.Tokenize(input)
-	for _, token := range tokens {
-		if token.Class == tokenizer.DUMMY {
-			continue
+	fp, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer fp.Close()
+	scanner := bufio.NewScanner(fp)
+
+	headerMap := make(map[string]struct{})
+
+	for scanner.Scan() {
+		input := scanner.Text()
+		input = string(norm.NFKC.Bytes([]byte(input)))
+
+		output := []string{}
+		tokens := t.Tokenize(input)
+		for _, token := range tokens {
+			if token.Class == tokenizer.DUMMY {
+				continue
+			}
+			if token.Features()[1] == "空白" {
+				continue
+			}
+			output = append(output, token.Surface)
+
+			headerMap[token.Surface] = struct{}{}
 		}
-		if token.Features()[1] == "空白" {
-			continue
-		}
-		output = append(output, token.Surface)
 	}
 
-	fmt.Println(output)
+	header := []string{}
+	for k := range headerMap {
+		header = append(header, k)
+	}
+
+	sort.Slice(header, func(i int, j int) bool {
+		return header[i] > header[j]
+	})
+	fmt.Printf("\"input\",")
+	fmt.Println(strings.Join(header, ","))
+
+	fp.Seek(0, 0)
+	scanner = bufio.NewScanner(fp)
+	for scanner.Scan() {
+		input := scanner.Text()
+		input = string(norm.NFKC.Bytes([]byte(input)))
+
+		output := make([]string, len(header))
+		tokens := t.Tokenize(input)
+		for _, token := range tokens {
+			if token.Class == tokenizer.DUMMY {
+				continue
+			}
+			if token.Features()[1] == "空白" {
+				continue
+			}
+
+			for i, h := range header {
+				if h == token.Surface {
+					output[i] = "1"
+				}
+			}
+		}
+		fmt.Printf("\"%s\",", input)
+		fmt.Println(strings.Join(output, ","))
+	}
 }
